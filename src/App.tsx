e12ba436e2632d899b4558e39ddedbebcff46796
유병우 <conner903@naver.com>
2023-07-29 13:20:46 +0900

    ✅: 카드 리스트 요소 추가 id test pass

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <div data-testid="kanban-board">
-        <div data-testid="card-list"></div>
-        <div data-testid="card-list"></div>
-        <div data-testid="card-list"></div>
+        <div data-testid="card-list-todo"></div>
+        <div data-testid="card-list-progress"></div>
+        <div data-testid="card-list-done"></div>
       </div>
     </div>
   );
