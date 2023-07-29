aa1e2d28e1814b2e73083dea92a64645bd8b336a
유병우 <conner903@naver.com>
2023-07-29 13:21:18 +0900

    ✅: 중복 test case 제거

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,12 +6,7 @@ test("App Test", () => {
   render(<App />);
   const kanbanboard = screen.getByTestId("kanban-board");
 
-  const cardLists = screen.getAllByTestId(/card-list/i);
-
   expect(kanbanboard).toBeInTheDocument();
-  expect(kanbanboard).toContainElement(cardLists?.[0]);
-
-  expect(cardLists).toHaveLength(3);
 
   const todoCardList = screen.getByTestId("card-list-todo");
   const progressCardList = screen.getByTestId("card-list-progress");
