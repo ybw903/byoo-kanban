5d46782fbe02f2b958a212fed2b2f4f55195f705
유병우 <conner903@naver.com>
2023-07-29 12:25:48 +0900

    ✅: 칸반보드 요소 test pass

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div data-testid="kanban-board"></div>
+    </div>
+  );
 }
 
 export default App;
